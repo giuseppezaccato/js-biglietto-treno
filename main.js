43c5644a2df8b1342ff0813b9be2b732d0b6d876
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-09 16:28:50 +0100

    CREATE: html/js + task*2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// fase preparatoria di raccolta dati dall'utente
-// -km richiesti per il viaggio come primo let 
-// -età del passeggero come secondo let
+//------RACCOLTA DATI DALL'UTENTE
+//  -km richiesti per il viaggio 
+//  -età del passeggero
 
 // ricorda sempre di fare in modo che i dati raccolti vengano trasformati in numeri,
 //  o almeno fai un controllo in modo che lo diventino (parseInt)
@@ -9,12 +9,30 @@ const age = parseInt(prompt("scrivi qui l'età del passeggero", 35))
 const km = parseInt(prompt("aggiungi qui quanti km intendi percorrere per sapere quanto costerà il tuo biglietto", 120))
 let price = km * 0.21
 
-// //sconto applicato in base all'età del passeggero
-// IF < 18 = price * 0.2
-// ELSE IF >= 65 = price * 0.4
-// ELSE = price
-// ricorda in ogni caso di SOVRASCRIVERE ilprezzo prima di fare il log, richiamando la variabile,
-// e non solo dandogliela nella condizione di IF
+//-----SCONTO APPLICATO IN BASE A ETà PASSEGGERO
+// IF < 18 = sconto 20%
+// ELSE IF >= 65 = sconto 40%
+// ELSE = prezzo pieno
+// ricorda in ogni caso di SOVRASCRIVERE il prezzo prima di fare il log,
+// richiamando la variabile, e non SOLO dandogliela nella condizione di IF
+
+if (age < 18) {
+    let price20 = price - (price * 0.2)
+    console.log(`in quanto minorenne costo biglietto =  ${price20} € `)
+    // alert("il prezzo del biglietto è di" + " " + price20.toFixed(2) + " €")
+
+} else if (age >= 65) {
+    let price40 = price - (price * 0.4)
+    console.log(`in quanto over 65 costo biglietto = ${price40} €`)
+    // alert("il prezzo del biglietto è di" + " " + price40.toFixed(2) + " €")
+
+} else {
+    console.log(`il prezzo del biglietto è di ${price} €`)
+    // alert("il prezzo del biglietto è di" + " " + price.toFixed(2) + " €")
+
+}
+
+
 
 
 
